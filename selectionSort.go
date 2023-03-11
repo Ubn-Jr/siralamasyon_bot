25fa614c2870b46ac7e49c6afe8ce2ec3fe3115b
Hatice Kübra Babaoğlu <100375578+haticebabaoglu@users.noreply.github.com>
2023-03-11 22:48:14 +0300

    Add files via upload

--- selectionSort.go
+++ selectionSort.go
@@ -7,7 +7,7 @@ func main(){
 var arr []int = []int{522,5,1,8,2,6,9,62,12}
 
 smallNumber := 0
-
+//Kucukten buyuge siralar 
 for i:= 0; i <len(arr); i ++ {
 	smallNumber = i
 	for j := i + 1 ; j < len(arr); j++ {
@@ -17,6 +17,17 @@ for i:= 0; i <len(arr); i ++ {
 	} 
     arr[i], arr[smallNumber] = arr[smallNumber], arr[i]
 }
+//Buyukten kucuge siralar 
+for i:= 0; i <len(arr); i ++ {
+	smallNumber = i
+	for j := i + 1 ; j > len(arr); j++ {
+		if arr[j] < arr[smallNumber]{
+			smallNumber = j 
+		}	
+	} 
+    arr[i], arr[smallNumber] = arr[smallNumber], arr[i]
+}
+
 fmt.Println(arr)
 
 }
