1dd764fc3029b06f5810cf1bca921ecbc368d059
Hatice Kübra Babaoğlu <100375578+haticebabaoglu@users.noreply.github.com>
2023-03-11 23:10:49 +0300

    Add files via upload

--- selectionSort.go
+++ selectionSort.go
@@ -2,32 +2,37 @@ package main
 
 import "fmt"
 
+
+
+func selectionSort(discordMesaji []int) {
+	smallNumber := 0
+	//Kucukten buyuge siralar 
+	for i:= 0; i <len(discordMesaji); i ++ {
+		smallNumber = i
+		for j := i + 1 ; j < len(discordMesaji); j++ {
+			if discordMesaji[j] < discordMesaji[smallNumber]{
+				smallNumber = j 
+			}	
+		} 
+		discordMesaji[i], discordMesaji[smallNumber] = discordMesaji[smallNumber], discordMesaji[i]
+	}
+	//Buyukten kucuge siralar 
+	for i:= 0; i <len(discordMesaji); i ++ {
+		smallNumber = i
+		for j := i + 1 ; j > len(discordMesaji); j++ {
+			if discordMesaji[j] > discordMesaji[smallNumber]{
+				smallNumber = j 
+			}	
+		} 
+		discordMesaji[i], discordMesaji[smallNumber] = discordMesaji[smallNumber], discordMesaji[i]
+	}
+	
+	fmt.Println(discordMesaji)
+	
+}
 func main(){
 
 var arr []int = []int{522,5,1,8,2,6,9,62,12}
 
-smallNumber := 0
-//Kucukten buyuge siralar 
-for i:= 0; i <len(arr); i ++ {
-	smallNumber = i
-	for j := i + 1 ; j < len(arr); j++ {
-		if arr[j] < arr[smallNumber]{
-			smallNumber = j 
-		}	
-	} 
-    arr[i], arr[smallNumber] = arr[smallNumber], arr[i]
-}
-//Buyukten kucuge siralar 
-for i:= 0; i <len(arr); i ++ {
-	smallNumber = i
-	for j := i + 1 ; j > len(arr); j++ {
-		if arr[j] > arr[smallNumber]{
-			smallNumber = j 
-		}	
-	} 
-    arr[i], arr[smallNumber] = arr[smallNumber], arr[i]
-}
-
-fmt.Println(arr)
-
+selectionSort(arr)
 }
